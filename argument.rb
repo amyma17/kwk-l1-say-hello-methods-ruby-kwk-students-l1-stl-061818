29a03b4636a3e60e5305e9929623a22f9bafc4fd
amyma17 <amyma171311@gmail.com>
2018-06-19 15:20:59 +0000

    automatically backed up by learn

---
+++ argument.rb
@@ -0,0 +1,4 @@
+def greeting(local_greeting="hello",your_name,my_name,time_of_day)
+  puts "#{local_greeting} #{your_name}, I am #{my_name} how's your #{time_of_day} been?"
+end 
+greeting("Karlie","Amy","day")
---
+++ morning_routine.rb
@@ -0,0 +1,10 @@
+def morning_routine 
+  puts "wake up"
+  puts "check phone"
+  puts "get dressed"
+  puts "brush my teeth"
+  puts "wash my face"
+  puts "eat breakfast"
+end 
+morning_routine
+morning_routine 
